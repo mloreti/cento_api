3eba56b6c7bc6c02f341070848f3132d0e93fec5
Mickey Loreti <mloreti10@gmail.com>
2017-08-01 23:03:58 -0700

    Add jbuilder to render json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-  before_action :authenticate_user
   # devise defines this, while knock is using `method_missing`
   undef_method :current_user
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user, only: [:show]
   def show
-    user = User.find(params[:id])
-    if user == current_user
-      render json: User.find(params[:id])
-    end
+    @user = User.find(params[:id])
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: 201
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 201
     else
-      render json: { errors: user.errors }, status: 422
+      render json: { errors: @user.errors }, status: 422
     end
   end
 
