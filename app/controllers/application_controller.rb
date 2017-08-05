c315d42b742aac9c5003d1164d8624e419ec245b
Mickey Loreti <mloreti10@gmail.com>
2017-08-05 00:53:17 -0700

    Respond to JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
   # devise defines this, while knock is using `method_missing`
   undef_method :current_user
+  respond_to :json
 end
